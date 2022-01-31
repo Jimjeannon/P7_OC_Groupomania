67a276c6d7def31a4995459da370296d652b8175
Jimjeannon <jim.jeannon@gmail.com>
2022-01-31 17:37:35 +0100

    build limit middlwar for password

---
+++ Backend/middlewar/limit.js
@@ -0,0 +1,12 @@
+// Utulisation de limit rate express pour limiter les nb d'essais mdt 
+const limit = require("express-rate-limit")
+
+const limiter = limit({
+    windowMs: 10 * 60 * 1000,
+    max: 5,
+    message: "Compte est bloqué 10min, nombre d'essais 5 maximum",
+    standardHeaders: true, 
+	legacyHeaders: false,
+})
+
+module.exports = { limiter }
